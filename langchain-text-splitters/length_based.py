0d03ac6bd7a49d3906b330ada89f1edd81443556
Talha Ghauri <talhaghauridev@gmail.com>
2025-07-22 19:07:11 +0500

    init langchain text splitters

---
+++ langchain-text-splitters/length_based.py
@@ -0,0 +1,13 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("./langchain-text-splitters/dl_curriculum.pdf")
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=0, separator="")
+
+
+result = splitter.split_documents(docs)
+
+print(result[1].page_content)
